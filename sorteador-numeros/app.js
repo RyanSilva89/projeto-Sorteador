0fd74d1e28b8e86f586e0329fcaf7f712fb296ff
Ryan Silva. <r.sfariaa89@gmail.com.>
2025-07-05 09:27:33 +0900

    Recuperando os campos e os valores(usando .value).

---
+++ sorteador-numeros/app.js
@@ -0,0 +1,11 @@
+function sortear(){
+    let quantidade = document.getElementById('quantidade').value;
+    let doNumero = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value;
+     
+
+   alert(`Quantidade :${quantidade}`);
+   alert(`Do número :${doNumero}`); 
+   alert(`até o número :${ate}`);
+}
+

dc66261ee9bfa2a099e7ea9b11bac812f9ae6022
Ryan Silva. <r.sfariaa89@gmail.com.>
2025-07-12 11:05:54 +0900

    feat: implementação de proteção no sorteio e validações de entrada
    - Adiciona verificação para impedir que o número inicial seja maior que o final
    - Bloqueia sorteios com quantidade superior ao intervalo permitido
    - Garante números únicos no sorteio, evitando repetições
    - Estrutura o código de forma clara e reutilizável
    - Atualiza a interface com resultado formatado
    - Alterna o estado do botão de reinício conforme necessário

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -2,13 +2,31 @@ function sortear(){
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let doNumero = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
-     
+    
+    //verificação
+const intervalo=  ate - doNumero + 1;
+
+   if(doNumero>ate){
+        alert("verifique se está preenchendo os campos corretamente");
+        return;
+
+    }
+   if(quantidade>intervalo){
+            alert(`Você está tentando sortear ${quantidade} números, mas o intervalo entre ${doNumero} e ${ate} só permite ${intervalo} números únicos.`);
+return;
+   }
+
+   
+  
+//Sorteio     
 let sorteados= [];
 let numero;
+
    for(let i=0; i < quantidade; i++){
     numero= obterNumeroAleatorio(doNumero,ate);
     while(sorteados.includes(numero)){
         numero = obterNumeroAleatorio (doNumero,ate);
+        
     }
     sorteados.push(numero);
    }
